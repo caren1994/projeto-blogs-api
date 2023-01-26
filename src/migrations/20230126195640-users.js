2936c1c364ee09b9eaee8cdd64f875966e314289
caren <carenoliveirasilvapontes@gmail.com>
2023-01-26 16:26:52 -0500

    req 1 e 2

---
+++ src/migrations/20230126195640-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize)=> {
+   await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        allowNull: false,
+        field:'display_name',
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+
+      },
+    });
+  },
+
+  async down(queryInterface, _Sequelize) {
+    return queryInterface.dropTable('users');
+  },
+};
---
+++ src/models/User.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      displayName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      image:{
+        allowNull: false,
+        type: DataTypes.STRING,
+      }
+    },
+    {
+      timestamps: false,
+      tableName: 'users',
+      underscored: true,
+    });
+    User.associate = (models) => {
+      User.hasMany(models.BlogPost, {
+        foreignKey: 'userId',
+        as: 'blogposts',
+      });
+    };
+    return User;
+  };
+  
