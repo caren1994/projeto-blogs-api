b905459c3af6299d1d72927029cad9a694dbef96
caren <carenoliveirasilvapontes@gmail.com>
2023-01-27 18:16:57 -0500

    req 11

---
+++ src/models/BlogPost.js
@@ -0,0 +1,46 @@
+module.exports=(sequelize,DataTypes)=>{
+  const BlogPost =sequelize.define('BlogPost',{
+    id:{
+      allowNull:false,
+      type:DataTypes.INTEGER,
+      autoIncrement:true,
+      primaryKey:true,
+    },
+    title:{
+      allowNull:false,
+      type:DataTypes.STRING,
+
+    },
+    content:{
+    allowNull:false,
+    type:DataTypes.STRING,
+    },
+    published:{
+      allowNull:false,
+      type:DataTypes.DATE,
+      defaultValue:sequelize.literal('CURRENT_TIMESTAMP'),
+
+    },
+    updated:{
+      allowNull:false,
+      type:DataTypes.DATE,
+      defaultValue:sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    userId:{
+      allowNull:false,
+      type:DataTypes.INTEGER,
+      foreignKey:true,
+      field:'user_id'
+    },
+
+  },
+  {
+    timestamp:false,
+    underscored:true,
+    tableName:'blog_posts',
+  });
+  BlogPost.associate=(models)=>{
+    BlogPost.belongsTo(models.User,{foreignKey:'userId',as:'user'})
+  }
+  return BlogPost;
+}
--- src/models/User.js
+++ src/models/User.js
@@ -28,12 +28,12 @@ module.exports = (sequelize, DataTypes) => {
       tableName: 'users',
       underscored: true,
     });
-    // User.associate = (models) => {
-    //   User.hasMany(models.BlogPost, {
-    //     foreignKey: 'userId',
-    //     as: 'blogposts',
-    //   });
-    // };
+    User.associate = (models) => {
+      User.hasMany(models.BlogPost, {
+        foreignKey: 'userId',
+        as: 'blogposts',
+      });
+    };
     return User;
   };
   
